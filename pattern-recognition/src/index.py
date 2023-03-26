4690581e530d6eeaacfc58342a0dec32409095f6
k.berdelis <k.berdelis@deepsea.ai>
2023-03-26 19:03:58 +0300

    pattern-recognition/fix-object-models

--- pattern-recognition/src/index.py
+++ pattern-recognition/src/index.py
@@ -1,35 +1,72 @@
 import numpy as np
-#scripting ond data to process them
+
 # Load the .npy file into a numpy array
+# and parse it into obj[]
 data = np.load('Dataset.npy')
 
-
-# #processing the data
 sortData = np.sort(data)
 lenData = len(data)
 
+actualData = sortData[:10]
+"""
+ Spactating & Processing the data
 
-# # spactating the data
+
+"""
 # # Print the first 10 rows of the array
 # first_10_rows = data[:10]
 # print(first_10_rows, "\ntype of row: ", type(data[0]))
 
-objects = []
+# objects = []
 
-# Split each string in the data array and create an object with the values
-for item in sortData:
+# # Split each string in the data array and create an object with the values
+# for item in sortData:
+#     values = item.split(',')
+#     obj = {
+#         'userId': values[0],
+#         'movieId': values[1],
+#         'rating': values[2],
+#         'date': values[3]
+#     }
+#     objects.append(obj)
+
+# # Print unique movie IDs and user IDs
+# unique_movies = set(obj['movieId'] for obj in objects)
+# unique_users = set(obj['userId'] for obj in objects)
+user_data = {}
+all_movies = set()
+for item in actualData:
     values = item.split(',')
-    obj = {
-        'userId': values[0],
-        'movieId': values[1],
-        'rating': values[2],
-        'date': values[3]
-    }
-    objects.append(obj)
+    userId = values[0]
+    movieId = values[1]
+    rating = int(values[2])
+    all_movies.add(movieId)
+    if userId in user_data:
+        user_data[userId]['rating'][movieId] = rating
+    else:
+        user_data[userId] = {
+            'rating': {movieId: rating},
+            'movies': []
+        }
 
-# Print unique movie IDs and user IDs
-unique_movies = set(obj['movieId'] for obj in objects)
-unique_users = set(obj['userId'] for obj in objects)
+# Create a list of objects with sorted movie IDs, all movie ratings, and user ID
+user_list = []
+for userId in user_data:
+    user_movies = sorted(list(all_movies))
+    user_rating = []
+    for movie in user_movies:
+        if movie in user_data[userId]['rating']:
+            user_rating.append(user_data[userId]['rating'][movie])
+        else:
+            user_rating.append(0)
+    user_list.append({
+        'userId': userId,
+        'movies': user_movies,
+        'rating': user_rating
+    })
 
-
+# Print the resulting user list
+print(len(user_list[0]["movies"]))
+print(len(user_list[1]["movies"]))
+# print(list(user_data.keys())[1]['movies'])
 
